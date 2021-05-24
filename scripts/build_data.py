abc170ff42189c24b3dd89c5668b7b7ec7ae5c80
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-05-24 15:10:01 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,13 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_data(df, context):
+  if 'SERIES' in context['meta']:
+    df['SERIES']=context['meta']['SERIES']
+  return df
+    
+def alter_meta(meta, context):
+  if 'SERIES' in meta
+    del meta['SERIES']
+  return meta
+
+open_sdg_build(config='config_data.yml', alter_data=alter_data, alter_meta=alter_meta)
